326c9ad34026d830c27c4847b7871b17e16f77e1
Raphael Vincent Quiestas <75572815+rvquiestas@users.noreply.github.com>
2024-11-09 20:20:59 +0800

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -10,7 +10,7 @@ const authUser = async (req, res, next) => {
 
     try {
         
-        const token_decode = jwt.verify(token, process.env.JWT_SECRET)
+        const token_decode = jwt.verify(token, "mgwrpc")
         req.body.userId = token_decode.id
         next()
 
