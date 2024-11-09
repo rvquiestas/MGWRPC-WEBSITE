ef4a6ac0574c483e2b487b2472af56c27388fa61
Raphael Vincent Quiestas <75572815+rvquiestas@users.noreply.github.com>
2024-11-09 19:39:17 +0800

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -10,7 +10,7 @@ const ShopContextProvider = (props) => {
 
     const currency = '₱';
     const delivery_fee = 100;
-    const backendUrl = import.meta.env.VITE_BACKEND_URL //backend to frontend
+    const backendUrl = "https://mgwrpc-backend.vercel.app" //backend to frontend
     const [search,setSearch] = useState('');
     const [showSearch,setShowSearch] = useState(false);
     const [cartItems,setCartItems] = useState({});
