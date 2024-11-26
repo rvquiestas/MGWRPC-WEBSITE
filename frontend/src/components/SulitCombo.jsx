c494094ac601e451cf09442830558ef7bfd4e4fb
rvquiestas <rvquiestas@gmail.com>
2024-11-26 22:58:05 +0800

    adjust available stock - home

--- frontend/src/components/SulitCombo.jsx
+++ frontend/src/components/SulitCombo.jsx
@@ -1,35 +1,41 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '../context/ShopContext'
-import Title from './Title';
-import ProductItem from './ProductItem';
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const SulitCombo = () => {
+  const { products } = useContext(ShopContext);
+  const [sulitCombo, setSulitCombo] = useState([]);
 
-    const { products } = useContext(ShopContext);
-    const[ sulitCombo, setSulitCombo ] = useState([]);
+  useEffect(() => {
+    // Include only those with category "Sulit Combo"
+    const filteredProducts = products
+      .filter((item) => item.category === "Sulit Combo")
+      .slice(0, 10);
+    setSulitCombo(filteredProducts);
+  }, [products]);
 
-    useEffect(() => {
-        // Include only those with category "Sulit Combo"
-        const filteredProducts = products.filter(item => item.category === "Sulit Combo").slice(0, 10);
-        setSulitCombo(filteredProducts);
-      }, [products]);
+  return (
+    <div className="my-10">
+      <div className="text-left py-8 text-2xl">
+        <Title text1={"SULIT"} text2={"COMBOS"} />
+      </div>
 
-    return (
-        <div className='my-10'>
-            <div className='text-left py-8 text-2xl'>
-                <Title text1={'SULIT'} text2={'COMBOS'} />
-            </div>
-    
-        {/* Products */}
-            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 gap-y-6'>
-            {
-              sulitCombo.map((item,index)=>(
-                <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
-              ))
-            }
-          </div>
-        </div>
-      )
-}
+      {/* Products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 gap-y-6">
+        {sulitCombo.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+            availableStock={item.availableStock}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default SulitCombo
+export default SulitCombo;
--- frontend/src/components/SulitPC.jsx
+++ frontend/src/components/SulitPC.jsx
@@ -1,34 +1,38 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '../context/ShopContext'
-import Title from './Title';
-import ProductItem from './ProductItem';
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const SulitPC = () => {
+  const { products } = useContext(ShopContext);
+  const [sulitPC, setSulitPC] = useState([]);
 
-    const { products } = useContext(ShopContext);
-    const[ sulitPC, setSulitPC ] = useState([]);
-
-    useEffect(()=>{
-        const bestProduct = products.filter((item)=>(item.sulitpc));
-        setSulitPC(bestProduct.slice(0,5))
-    },[products])
+  useEffect(() => {
+    const bestProduct = products.filter((item) => item.sulitpc);
+    setSulitPC(bestProduct.slice(0, 5));
+  }, [products]);
 
   return (
-    <div className='my-10'>
-        <div className='text-left py-8 text-2xl'>
-            <Title text1={'RECOMMENDED'} text2={'BUILDS'} />
-        </div>
+    <div className="my-10">
+      <div className="text-left py-8 text-2xl">
+        <Title text1={"RECOMMENDED"} text2={"BUILDS"} />
+      </div>
 
-    {/* Products */}
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 gap-y-6'>
-        {
-          sulitPC.map((item,index)=>(
-            <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
-          ))
-        }
+      {/* Products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 gap-y-6">
+        {sulitPC.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+            availableStock={item.availableStock}
+          />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SulitPC
+export default SulitPC;
